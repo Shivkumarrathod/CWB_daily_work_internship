2368ffe05282672607119d7f8bab5b42ccc3999c
Shivkumarrathod <shivarathod244@gmail.com>
2025-07-30 16:22:18 +0530

    first commit

---
+++ Practice/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		int age=20;
+		double heightInMeters=5.7;
+		char firstInitial='M';
+		boolean isCurrentStudent=true;
+		String fullName="Shivkumar RAthod";
+		System.out.println("Age="+age);
+		System.out.println("heightInMeters="+heightInMeters);
+		System.out.println("firstInitial="+firstInitial);
+		System.out.println("isCurrentStudent="+isCurrentStudent);
+		System.out.println("fullName="+fullName);
+	}
+ 
+}
